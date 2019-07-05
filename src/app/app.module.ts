ba7838db12a704b3e0086ba2110f261be561a05d
Punnoose <punnoosekuttyj@yahoo.com>
2019-07-05 13:30:33 -0400

    added custom validator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SubscriptionValidatorDirective } from './subscription-validator.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSettingsFormComponent,
+    SubscriptionValidatorDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ButtonsModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
